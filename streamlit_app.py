bf371a69522de6046219f8dac165f5fccda7e575
Michael Condon <michael.condon@interworks.nl>
2023-11-06 20:01:44 +0100

    rename

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 import datetime
 from datetime import datetime as dt
-import streamlit as st
+import streamlit_app as st
 import plotly.graph_objects as go
 
 
